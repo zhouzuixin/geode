cc906d4b48b216a5a96db345c9bc1d11d84c0ea4
Udo Kohlmeyer <ukohlmeyer@gmail.com>
2018-12-06 13:39:13 -0800

    Interview baseline

---
+++ geode-core/src/main/kotlin/demo/Client.kt
@@ -0,0 +1,62 @@
+package demo/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import org.apache.geode.cache.client.ClientCacheFactory
+import org.apache.geode.cache.client.ClientRegionShortcut
+import java.util.*
+import java.util.stream.IntStream
+
+fun main(args: Array<String>) {
+    val properties = Properties().apply {
+        setProperty("mcast-port", "0")
+        setProperty("statistic-sampling-enabled", "true")
+    }
+    val cache = ClientCacheFactory(properties).addPoolLocator("10.118.19.22 ", 10334).create()
+//    val cache = ClientCacheFactory(properties).addPoolLocator(args[0], args[1].toInt()).addPoolLocator(args[2], args[3].toInt()).create()
+
+    val regionFactory = cache.createClientRegionFactory<String, String>(ClientRegionShortcut.PROXY)
+    regionFactory.setStatisticsEnabled(true)
+    val region1 = regionFactory.create("Region1")
+
+    val regionFactory2 = cache.createClientRegionFactory<String, String>(ClientRegionShortcut.PROXY)
+    regionFactory2.setStatisticsEnabled(true)
+    val region2 = regionFactory2.create("Region3")
+
+    val random = Random()
+    IntStream.range(0, Int.MAX_VALUE).parallel().forEach { count ->
+
+        region1[count.toString()] = region1.size.toString()
+
+        for (value in 0..random.nextInt(25)) {
+            region2[(value*count).toString()] = value.toString()
+        }
+
+        if (random.nextBoolean()) {
+            for (value in 0..random.nextInt(35)) {
+                region1[count.toString()]
+                region2.destroy((value*count).toString())
+            }
+        } else {
+            for (value in 0..random.nextInt(10)) {
+                region2[count.toString()]
+                region1.destroy((value*count).toString())
+            }
+        }
+        Thread.sleep(100)
+
+        println("Processed value $count")
+
+    }
+}
---
+++ geode-core/src/main/kotlin/demo/Server.kt
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package demo
+
+import org.apache.geode.cache.CacheFactory
+import org.apache.geode.distributed.ConfigurationProperties
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    val properties = Properties().apply {
+        set(ConfigurationProperties.LOCATORS, "10.118.19.22[10334]")
+        set(ConfigurationProperties.NAME, "server")
+        set(ConfigurationProperties.CACHE_XML_FILE, "cache123.xml")
+        set(ConfigurationProperties.BIND_ADDRESS, "10.118.19.22")
+        set(ConfigurationProperties.MCAST_PORT, "0")
+        set(ConfigurationProperties.STATISTIC_SAMPLING_ENABLED, "true")
+        set(ConfigurationProperties.JMX_MANAGER, "false")
+        set(ConfigurationProperties.JMX_MANAGER_START, "false")
+        set(ConfigurationProperties.ENABLE_CLUSTER_CONFIGURATION, "false")
+        set(ConfigurationProperties.USE_CLUSTER_CONFIGURATION, "false")
+        set(ConfigurationProperties.CONSERVE_SOCKETS, "false")
+        set(ConfigurationProperties.ENABLE_NETWORK_PARTITION_DETECTION, "false")
+    }
+    val cache = CacheFactory(properties).create()
+    cache.addCacheServer().apply {
+        port = 0
+        bindAddress = "10.118.19.22"
+        hostnameForClients = "10.118.19.22"
+    }.start()
+}
